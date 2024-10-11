d4e226d863253ee04839af5e8543417b0620d923
w0ahnder <woahnder@gmail.com>
2024-10-11 17:00:43 -0400

    created eclipse project

--- src/main/java/common/Convert.java
+++ src/main/java/common/Convert.java
@@ -1,26 +1,25 @@
 package common;
+
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 
 public class Convert {
 
-    private String path;
-    private TupleReader reader;
-    private PrintStream printStream;
+  private String path;
+  private TupleReader reader;
+  private PrintStream printStream;
 
+  public Convert(String path, PrintStream ps) throws FileNotFoundException {
+    this.path = path;
+    reader = new TupleReader(new File(path));
+    printStream = ps;
+  }
 
-    public Convert(String path, PrintStream ps) throws FileNotFoundException {
-        this.path = path;
-        reader= new TupleReader(new File(path));
-        printStream = ps;
+  public void bin_to_human() throws IOException {
+    Tuple t = reader.read();
+    while (t != null) {
+      printStream.println(t);
+      t = reader.read();
     }
-    public void bin_to_human() throws IOException {
-        Tuple t = reader.read();
-        while(t!=null){
-            printStream.println(t);
-            t = reader.read();
-        }
-        reader.reset();
-    }
+    reader.reset();
+  }
 }
--- src/test/java/TupleReadWriteTest.java
+++ src/test/java/TupleReadWriteTest.java
@@ -1,4 +1,12 @@
 import common.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 import jdk.jshell.spi.ExecutionControl;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
@@ -8,64 +16,51 @@ import operator.PhysicalOperators.Operator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
+public class TupleReadWriteTest {
+  private static List<Statement> statementList;
+  private static QueryPlanBuilder queryPlanBuilder;
+  private static Statements statements;
 
+  @BeforeAll
+  static void setupBeforeAllTests() throws IOException, JSQLParserException, URISyntaxException {
+    ClassLoader classLoader = TupleReadWriteTest.class.getClassLoader();
+    URI path = Objects.requireNonNull(classLoader.getResource("binary_samples/input")).toURI();
+    Path resourcePath = Paths.get(path);
 
+    DBCatalog.getInstance().setDataDirectory(resourcePath.resolve("db").toString());
 
-public class TupleReadWriteTest{
-    private static List<Statement> statementList;
-    private static QueryPlanBuilder queryPlanBuilder;
-    private static Statements statements;
+    URI queriesFile =
+        Objects.requireNonNull(classLoader.getResource("binary_samples/input/test_queries.sql"))
+            .toURI();
 
-    @BeforeAll
-    static void setupBeforeAllTests() throws IOException, JSQLParserException, URISyntaxException {
-        ClassLoader classLoader = TupleReadWriteTest.class.getClassLoader();
-        URI path = Objects.requireNonNull(classLoader.getResource("binary_samples/input")).toURI();
-        Path resourcePath = Paths.get(path);
+    statements = CCJSqlParserUtil.parseStatements(Files.readString(Paths.get(queriesFile)));
+    queryPlanBuilder = new QueryPlanBuilder();
+    statementList = statements.getStatements();
+  }
 
-        DBCatalog.getInstance().setDataDirectory(resourcePath.resolve("db").toString());
-
-        URI queriesFile =
-                Objects.requireNonNull(classLoader.getResource("binary_samples/input/test_queries.sql")).toURI();
-
-        statements = CCJSqlParserUtil.parseStatements(Files.readString(Paths.get(queriesFile)));
-        queryPlanBuilder = new QueryPlanBuilder();
-        statementList = statements.getStatements();
+  @Test
+  public void testBoat() throws ExecutionControl.NotImplementedException, IOException {
+    Operator plan = queryPlanBuilder.buildPlan(statementList.get(0));
+    TupleReader tr = new TupleReader(DBCatalog.getInstance().getFileForTable("Boats"));
+    TupleWriter tw = new TupleWriter("src/test/resources/binary_samples/BoatRead");
+    Tuple t = tr.read();
+    while (t != null) {
+      tw.write(t);
+      t = tr.read();
     }
+    tw.close();
+  }
 
-    @Test
-    public void testBoat() throws ExecutionControl.NotImplementedException, IOException {
-        Operator plan = queryPlanBuilder.buildPlan(statementList.get(0));
-        TupleReader tr = new TupleReader(DBCatalog.getInstance().getFileForTable("Boats"));
-        TupleWriter tw = new TupleWriter("src/test/resources/binary_samples/BoatRead");
-        Tuple t = tr.read();
-        while ( t!=null){
-            tw.write(t);
-            t = tr.read();
-        }
-        tw.close();
+  @Test
+  public void testSailors() throws ExecutionControl.NotImplementedException, IOException {
+    Operator plan = queryPlanBuilder.buildPlan(statementList.get(1));
+    TupleReader tr = new TupleReader(DBCatalog.getInstance().getFileForTable("Sailors"));
+    TupleWriter tw = new TupleWriter("src/test/resources/binary_samples/SailorsRead");
+    Tuple t = tr.read();
+    while (t != null) {
+      tw.write(t);
+      t = tr.read();
     }
-
-    @Test
-    public void testSailors() throws ExecutionControl.NotImplementedException, IOException {
-        Operator plan = queryPlanBuilder.buildPlan(statementList.get(1));
-        TupleReader tr = new TupleReader(DBCatalog.getInstance().getFileForTable("Sailors"));
-        TupleWriter tw = new TupleWriter("src/test/resources/binary_samples/SailorsRead");
-        Tuple t = tr.read();
-        while ( t!=null){
-            tw.write(t);
-            t = tr.read();
-        }
-        tw.close();
-    }
-
-
+    tw.close();
+  }
 }
