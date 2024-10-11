b9b69dc07534772a5c22b7285c1a8f234bc7edcd
w0ahnder <woahnder@gmail.com>
2024-10-11 12:52:55 -0400

    Copied over utilities folder from tuple logger branch

---
+++ src/main/java/utilities/DataGenerator.java
@@ -0,0 +1,49 @@
+package utilities;
+
+import common.Tuple;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class DataGenerator {
+    //range we want each value in a row to fall between
+    int range;
+
+    //total number of tuples we want
+    int total;
+
+    //how many columns in a tuple
+    int tupleSize;
+
+    //all the generated tuple
+    ArrayList<Tuple> data;
+    Random random;
+
+    public DataGenerator(int range, int size, int tupleSize){
+        this.range = range;
+        this.total = size;
+        this.tupleSize = tupleSize;
+        this.random = new Random();
+        generateTuples();
+    }
+
+    public void generateTuples() {
+        for (int i = 0; i < total; i += 1) {
+            StringBuilder tup = new StringBuilder();
+
+            for (int j = 0; i < tupleSize; i++) {
+                int num = random.nextInt(range) + 1;
+                tup.append(num);
+
+                if (j < tupleSize - 1) {
+                    tup.append(",");
+                }
+            }
+            data.add(new Tuple(tup.toString()));
+        }
+    }
+
+    public ArrayList<Tuple>  allTuples(){
+        return data;
+    }
+}
---
+++ src/main/java/utilities/Logger.java
@@ -0,0 +1,55 @@
+package utilities;
+
+import common.Tuple;
+import operator.PhysicalOperators.Operator;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logger {
+
+    private static Logger instance;
+    private BufferedWriter writer;
+
+    private Logger() {
+        try {
+            writer = new BufferedWriter(new FileWriter("debug.txt", true));
+        } catch (IOException e) {
+            System.err.println("Error initializing log file: " + e.getMessage());
+        }
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(Operator op) {
+        try {
+            Tuple t = op.getNextTuple();
+            while ( t != null) {
+                writer.write(t.toString());
+                writer.newLine();
+                writer.flush();
+                t = op.getNextTuple();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            if (writer != null) {
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Error closing log file: " + e.getMessage());
+        }
+    }
+}
