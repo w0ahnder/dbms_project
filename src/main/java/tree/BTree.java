0b9264caecdd07738dbe8ec5177e6ba198024db0
w0ahnder <woahnder@gmail.com>
2024-11-06 12:47:40 -0500

    Created leaf layer

--- src/main/java/tree/BTree.java
+++ src/main/java/tree/BTree.java
@@ -1,4 +1,85 @@
 package tree;
 
+import common.Tuple;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class BTree {
+
+    boolean clustered;
+    int attribute;
+    int d;
+
+    public BTree(boolean clust, int  col , int order){
+        clustered = clust;
+        attribute = col;
+        d = order;
+    }
+
+
+    //has to create index nodes
+    // TODO: CREATE INDEX LAYER
+
+    //has to create leaf layer
+    public ArrayList<Leaf> leafLayer(HashMap<Integer, ArrayList<Tuple>> data){
+
+        ArrayList<Leaf> leaves =  new ArrayList<>();
+        int tot = data.size();//total number of keys
+        int processed = 0;//number of keys we have processed
+
+        //each leaf node has d<= k <=2d
+        //to fill tree completely, each node gets 2d entries
+        //if 2d<k<3d have to handle differently
+
+        Integer[] key_arr = data.keySet().toArray(new Integer[tot]);
+        int start = 0;
+        //keep track of addresses for serializing later
+        int addr = 1; //pages for leaves start at 1
+        while(!( 2*d < tot && tot <3*d) && processed<tot ){
+            //means k>=2d  (so we can use 2d) , k<2d ( we have to use k), k>=3d (can use 2d)
+            int collect= (tot - processed) <2*d? (tot - processed) : 2*d; //how many keys to put in this node
+            int end = start + collect;
+            leaves.add( makeLeaf(data, start, end, key_arr, addr));
+            start = end;
+            processed = processed+collect;
+            addr++;
+        }
+        int rem  = tot-processed;
+        //now handle case when not enough to fill the last 2 leaves properly
+        if(2*d < rem && rem< 3*d){
+            int left = rem/2; //number of entries for second to last leaf node
+            int right = rem - left;
+            int end = start + left;
+            leaves.add(makeLeaf(data, start, end, key_arr, addr));
+            addr++;
+            start = end;
+            end = tot;
+            leaves.add(makeLeaf(data, start, end, key_arr, addr));
+        }
+
+        return leaves;
+
+    }
+
+
+    public Leaf makeLeaf(HashMap<Integer, ArrayList<Tuple>> data, int start, int end, Integer[] key_arr, int address){
+        //each leaf node has to have the < key, list> structure
+        ArrayList<Leaf> leaves = new ArrayList<>();
+        ArrayList<Integer> key_list = new ArrayList<>();
+        ArrayList< ArrayList<Tuple>> rid_lists = new ArrayList<>();
+        //keep keys separate from rids so easier to search for a specific search key
+        //key i corresponds to list i in
+        for(int i=start; i<end;i++){
+            int key = key_arr[i];
+            ArrayList<Tuple> list = data.get(key);
+
+            key_list.add(key);
+            rid_lists.add(list);
+        }
+
+        return new Leaf(key_list, rid_lists, address);
+    }
 }
--- src/main/java/tree/BulkLoad.java
+++ src/main/java/tree/BulkLoad.java
@@ -3,6 +3,7 @@ package tree;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -17,12 +18,14 @@ public class BulkLoad {
     public int attribute;
     public boolean clustered; //true if clustered
     public TupleReader reader;
+    public BTree tree;
     public BulkLoad(File table, int order, int col, boolean clustered) throws FileNotFoundException {
     d = order;
     this.table = table;
     attribute  = col;
     this.clustered = clustered;
     reader = new TupleReader(table);
+    tree = new BTree(clustered, col, order);
 
     }
 
@@ -46,9 +49,23 @@ public class BulkLoad {
             data.put(key,loc);
         }
 
+        //have to create leaf layer
+        ArrayList<Leaf> leaves = tree.leafLayer(data);
+        printLeaves(leaves);
+        //now have to make index layer
+    }
+    public void printLeaves(ArrayList<Leaf> leaves) throws FileNotFoundException {
+        PrintStream ps = new PrintStream(new File("src/test/resources/samples-2/bulkload/Boats.E_bulk"));
+        for(Leaf leaf: leaves){
+            ps.println("Leaf Node");
+            String s = leaf.toString();
+            ps.println(s);
+        }
+        ps.close();
     }
 
 }
+
 class PRCompare implements Comparator<Tuple>{
 
     public int compare(Tuple t1, Tuple t2){
