efdbd15b8c65a8eccc3bd854db2494d9ddad6790
w0ahnder <woahnder@gmail.com>
2024-11-06 15:28:05 -0500

    did index layers, now have to do root and multiple index layers

--- src/main/java/tree/Leaf.java
+++ src/main/java/tree/Leaf.java
@@ -4,19 +4,27 @@ import common.Tuple;
 
 import java.util.ArrayList;
 
-public class Leaf {
+public class Leaf extends Node{
 
     public ArrayList<Integer>  keys;
     public ArrayList< ArrayList<Tuple>> rids;
     public int addr;
 
     public Leaf(ArrayList<Integer> keys, ArrayList< ArrayList<Tuple>> rid, int addr){
+        super(new ArrayList<>(), addr);
         this.keys = keys;
         this.rids = rid;
         this.addr = addr;
     }
 
 
+    public ArrayList<Integer> getKeys(){
+        return keys;
+    }
+
+    public int smallest(){
+        return keys.getFirst();
+    }
     public String toString(){
         String s = "";
         for(int i=0; i< keys.size();i++){
