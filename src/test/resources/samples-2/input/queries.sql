bb6721f6276dac7e717374aa35aab731d69eacbc
w0ahnder <woahnder@gmail.com>
2024-11-08 19:14:34 -0500

    query 8 throwing compiler error -1 out of bounds, probly in external sort

--- src/test/resources/samples-2/input/queries.sql
+++ src/test/resources/samples-2/input/queries.sql
@@ -1,1 +1,8 @@
 SELECT * FROM Sailors WHERE Sailors.A < 4000;
+SELECT * FROM Sailors WHERE Sailors.A <= 4000;
+SELECT * FROM Sailors WHERE Sailors.A <= 1000 AND Sailors.B<3000;
+SELECT Sailors.A, Boats.E FROM Sailors, Boats WHERE Sailors.A < 4000 AND Boats.E <300;
+SELECT Sailors.A, Boats.E FROM Sailors, Boats WHERE Sailors.A <= 4000 AND Boats.E <=35;
+SELECT Sailors.A, Sailors.B, Boats.E FROM Sailors, Boats WHERE Sailors.A <= 4000 AND Boats.E <=35 AND Sailors.A!=Sailors.B;
+SELECT Sailors.A, Boats.E FROM Sailors, Boats WHERE Sailors.A <= 4000 AND Boats.E <=35 ORDER BY Sailors.A, Boats.E;
+SELECT DISTINCT Sailors.A, Boats.E FROM Sailors, Boats WHERE Sailors.A <= 4000 AND Boats.E <=35 ORDER BY Sailors.A, Boats.E;
